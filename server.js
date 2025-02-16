5c99946347497b5bffd69aa0b632f2101c3c1171
Breezy2011 <128012745+Breezy2011@users.noreply.github.com>
2025-02-16 10:59:02 -0800

    server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname)));
 
+// Serve the main HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'movie_finder.html'));
 });
