882fb9189d252e98d1a762f561df2458d5689c56
Breezy2011 <128012745+Breezy2011@users.noreply.github.com>
2025-02-16 11:02:10 -0800

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-// Serve the main HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'movie_finder.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(process.env.PORT || 3000);
