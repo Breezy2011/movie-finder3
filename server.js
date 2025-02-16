e50a118108b7a4705d7b3a007ff43fa7fc6e2f8a
Breezy2011 <128012745+Breezy2011@users.noreply.github.com>
2025-02-16 11:35:54 -0800

    server.js

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Basic static file serving
-app.use(express.static(__dirname));
-
-// Simple health check
-app.get('/healthz', (req, res) => {
-    res.status(200).send('healthy');
-});
+app.use(express.static(path.join(__dirname)));
 
 // Main route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'movie_finder.html'));
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log('Server is running on port:', PORT);
+// Other routes
+app.get('/school', (req, res) => {
+    res.sendFile(path.join(__dirname, 'school.html'));
 });
+
+app.get('/1v1game', (req, res) => {
+    res.sendFile(path.join(__dirname, '1v1game.html'));
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+// For Vercel, we export the app instead of calling listen
+module.exports = app;
