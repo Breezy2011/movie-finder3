b7578d6a4b92d39ea61ccc93ab07fbc372cc6dc9
Breezy2011 <128012745+Breezy2011@users.noreply.github.com>
2025-02-16 11:04:52 -0800

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'movie_finder.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.get('/school', (req, res) => {
+    res.sendFile(path.join(__dirname, 'school.html'));
+});
+
+app.get('/1v1game', (req, res) => {
+    res.sendFile(path.join(__dirname, '1v1game.html'));
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
